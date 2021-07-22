89c5515fe1e11521a4913e622867a371d2894d53
Vinícius Alves Hax <viniciushax@gmail.com>
2021-07-22 16:24:04 -0300

    Adicionando exemplo de data em Python

---
+++ python/date.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+#Pegando a data de hoje em Python
+#https://stackoverflow.com/questions/32490629/getting-todays-date-in-yyyy-mm-dd-in-python
+datetime.today().strftime('%Y-%m-%d')
