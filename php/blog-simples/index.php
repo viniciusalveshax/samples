38266a6d733754c34f00bca224fde33c2040f14d
Vinícius Alves Hax <viniciushax@gmail.com>
2024-01-10 12:23:13 -0300

    Adicionando uma tela inicial que deverá mostrar as postagens

---
+++ php/blog-simples/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require "header.php";
+
+$usuario = $grupo;
+$senha = $grupo;
+$base_de_dados = $grupo;
+
+echo "<h1>Postagens</h1>";
+
+$pdo = new PDO('mysql:host=localhost;dbname=' . $grupo, $grupo, $grupo);
+
+
+$stmt = $pdo->query("SELECT * FROM postagens");
+while ($linha = $stmt->fetch()) {
+	$titulo = $linha["titulo"];
+	$texto = $linha["texto"];
+	echo "<h2>$titulo</h2>";
+	echo "<p>$texto</p>";
+}
+
+// Fecha a conexão
+$pdo = null;
+	
+
+?>
